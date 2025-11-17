e12c77a2619faa309003477c8026367fbe252729
masrurprem <masrur.kuet@gmail.com>
2025-11-17 19:01:58 +0600

    prisma and backend folders setup

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
-require("dotenv").config();
 const express = require("express");
+require("dotenv").config();
+const prisma = require("./src/db/prisma-client");
 const app = express();
 
 // Middleware
@@ -10,22 +11,13 @@ app.use(express.static("public"));
 // // Routes
 // app.use("/api", require("./routes/api"));
 
-// Home route
-app.get("/", (req, res) => {
-  res.json({ message: "Welcome to Express API" });
-});
-
 // Error handling middleware
 app.use((err, req, res, next) => {
   console.error(err.stack);
   res.status(500).json({ error: "Something went wrong!" });
 });
 
-// 404 handler
-app.use((req, res) => {
-  res.status(404).json({ error: "Route not found" });
-});
-
+//
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
